416ed0406dcaea7f7bd13512c5f1bfe6718dc506
Kimeel <kimeelsooknunan@gmail.com>
2025-03-31 16:21:09 +0100

    added basic login page

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Kimi'}
+    user = {'username': 'Kimeel'}
     
     posts = [
         {
@@ -18,3 +19,8 @@ def index():
     ]
     
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
